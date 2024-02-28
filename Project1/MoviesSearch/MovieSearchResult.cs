c50185038b58c948cf4250029f753d648dff7a9f
Sandro Pastorini <sandropastorini@gmail.com>
2024-02-28 20:31:44 -0300

    enable front-back communication
    - properly configured proxy for forwarding requests
    - added content loading logic to frontend
    - added mock controller to backend

---
+++ Project1/MoviesSearch/MovieSearchResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Project1.MoviesSearch
+{
+    public class MovieSearchResult
+    {
+        public UInt16 resultCount { get; set; }
+        public IList<MovieSearchResultItem> results { get; set; }
+
+        public MovieSearchResult(UInt16 resultCount, IList<MovieSearchResultItem> results) {
+            this.resultCount = resultCount;
+            this.results = results;
+        }
+    }
+}
---
+++ Project1/MoviesSearch/MovieSearchResultItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Project1.MoviesSearch
+{
+    public class MovieSearchResultItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+
+        public MovieSearchResultItem(int id, string title, string description, string imgUrl)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            ImgUrl = imgUrl;
+        }
+    }
+}
